67a8ca8d5d592e9a3e78dbbefe4fbe4b571ec9c3
mpouedras <Mathieu.POUEDRAS@cgos.asso.fr>
2017-03-03 16:40:43 +0100

    better

--- src/Percolation.java
+++ src/Percolation.java
@@ -29,18 +29,34 @@ public class Percolation {
         Percolation percolation = new Percolation(4);
 
         percolation.open(2, 3);
-        percolation.open(3, 3);
-        percolation.open(3, 2);
-        percolation.open(4, 2);
-        percolation.open(1, 3);
-
-        System.out.println(percolation.isFull(1, 3));
-
         System.out.println(percolation.printGrid());
-
-
         System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+        System.out.println();
 
+        percolation.open(3, 3);
+        System.out.println(percolation.printGrid());
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+        System.out.println();
+
+        percolation.open(3, 2);
+        System.out.println(percolation.printGrid());
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+        System.out.println();
+
+        percolation.open(4, 2);
+        System.out.println(percolation.printGrid());
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+        System.out.println();
+
+        percolation.open(1, 3);
+        System.out.println(percolation.printGrid());
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+        System.out.println();
     }
 
     public void open(int row, int col) {
@@ -55,10 +71,6 @@ public class Percolation {
         this.grid[row - 1][col - 1] = true;
         openSitesCount++;
         unionWithNeighbor(row, col);
-        if (this.isFull(row, col)) {
-            this.isPercolating = true;
-        }
-
     }
 
     private void unionWithNeighbor(int row, int col) {
@@ -74,13 +86,13 @@ public class Percolation {
                 this.unionFind.union(getSiteNumber(row, col), getSiteNumber(row + 1, col));
             }
         }
-        // east
+        // west
         if (col > 1) {
             if (this.isOpen(row, col - 1)) {
                 this.unionFind.union(getSiteNumber(row, col), getSiteNumber(row, col - 1));
             }
         }
-        // west
+        // east
         if (col < this.gridSize) {
             if (this.isOpen(row, col + 1)) {
                 this.unionFind.union(getSiteNumber(row, col), getSiteNumber(row, col + 1));
@@ -109,15 +121,10 @@ public class Percolation {
             throw new IllegalArgumentException(buildErrorMessage(col));
         }
         int component = unionFind.find(getSiteNumber(row, col));
-        for (int topSite = 0; topSite < this.gridSize - 1; topSite++) {
+        for (int topSite = 0; topSite < this.gridSize; topSite++) {
             int topComponent = unionFind.find(getSiteNumber(1, topSite + 1));
             if (component == topComponent) {
-                for (int botomSite = 0; botomSite < this.gridSize - 1; botomSite++) {
-                    int bottomComponent = unionFind.find(getSiteNumber(this.gridSize, botomSite + 1));
-                    if (component == bottomComponent) {
-                        return true;
-                    }
-                }
+                return true;
             }
         }
 
@@ -129,7 +136,12 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return this.isPercolating;
+        for (int bottomSite = 0; bottomSite < this.gridSize; bottomSite++) {
+            if (isFull(this.gridSize, bottomSite + 1)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private int getSiteNumber(int row, int col) {
